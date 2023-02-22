b818728dd0dd3db2bda9088e6420d9216460193b
fred-vogt-dod <fvogt@doctorondemand.com>
2023-02-22 15:30:10 -0800

    Fix 5.10 kernel version upgrade to support live kernel patching

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -15,9 +15,12 @@ case $(uname -r) in
         ;;
     *)
         echo "use linux kernel 5.10"
+        if yum versionlock | grep 'kernel'; then
+            yum versionlock delete 'kernel'
+        fi
         amazon-linux-extras disable kernel-5.4
         amazon-linux-extras install kernel-5.10 -y
-        rpm -qa | grep kernel
+        rpm -qa | grep 'kernel'
 
         echo "rebooting the instance"
         reboot
